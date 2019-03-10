0e56513a7f0336fdf18fadb05b748f678917eb55
SqrtMinusOne <thexcloud@gmail.com>
2019-03-10 17:00:43 +0300

    Bezier curve controls

--- src/bezier.py
+++ src/bezier.py
@@ -10,11 +10,11 @@ from main import cart2pol, pol2cart, random_rgb
 
 
 class ControlElement:
-    p = 0.05
+    p = 0.02
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, color=None):
         self.x, self.y = x, y
-        self.color = random_rgb()
+        self.color = random_rgb() if color is None else color
 
     def get_points(self):
         yield self.x - self.p / 2, self.y - self.p / 2
@@ -37,7 +37,7 @@ class ControlElement:
         ))
 
     def paint(self):
-        GL.glBegin(GL.GL_QUADS)
+        GL.glBegin(GL.GL_LINE_LOOP)
         GL.glColor4d(*self.color)
         for point in self.get_points():
             GL.glVertex2d(*point)
@@ -57,6 +57,7 @@ class MainWindow(QMainWindow, Ui_BezierWindow):
         self.openGLWidget.mouseMoveEvent = self.onOpenGLWidgetMouseMoved
         self.openGLWidget.mousePressEvent = self.onOpenGLWidgetMousePressed
         self.openGLWidget.mouseReleaseEvent = self.onOpenGLWidgetMouseReleased
+        self.updateButton.clicked.connect(self.generateControls)
         self.generateControls()
 
     def onOpenGLWidgetMousePressed(self, event):
@@ -88,7 +89,9 @@ class MainWindow(QMainWindow, Ui_BezierWindow):
         GL.glLoadIdentity()
 
     def generateControls(self):
-        self.controls = [ControlElement(np.random.random(), np.random.random()) for _ in range(5)]
+        color = random_rgb()
+        self.controls = [ControlElement(np.random.random(), np.random.random(), color) for _ in range(4)]
+        self.openGLWidget.update()
 
     def paintGL(self):
         self.loadScene()
@@ -100,6 +103,46 @@ class MainWindow(QMainWindow, Ui_BezierWindow):
 
     def drawStuff(self):
         [c.paint() for c in self.controls]
+        self.paintBezier(*[c.center() for c in self.controls[0:4]])
+        self.paintControlLines()
+
+    def paintControlLines(self):
+        GL.glColor4d(1, 1, 1, 1)
+        GL.glBegin(GL.GL_LINE_STRIP)
+        [GL.glVertex2d(*c.center()) for c in self.controls]
+        GL.glEnd()
+        GL.glFinish()
+
+    def paintBezier(self, p1, p2, p3, p4):
+        line11, line12, line13 = [], [], []
+
+        # GL.glBegin(GL.GL_POINTS)
+        # GL.glVertex2d(*p4)
+        # GL.glVertex2d(*p3)
+        # GL.glEnd()
+
+        GL.glBegin(GL.GL_LINE_STRIP)
+
+        for i in range(100):
+            line11.append((p2 - p1) / 100 * i + p1)
+            line12.append((p3 - p2) / 100 * i + p2)
+            line13.append((p4 - p3) / 100 * i + p3)
+
+        line21, line22 = [], []
+
+        for i in range(100):
+            dot1 = line11[i]
+            dot2 = line12[i]
+            dot3 = line13[i]
+            line21.append(((dot2 - dot1) / 100 * i + dot1))
+            line22.append(((dot3 - dot2) / 100 * i + dot2))
+
+        for i in range(100):
+            dot21 = line21[i]
+            dot22 = line22[i]
+            GL.glVertex2d(*((dot22 - dot21) / 100 * i + dot21))
+
+        GL.glEnd()
         GL.glFinish()
 
 
