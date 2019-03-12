c6b44ee3ac41b98286bd837430a40c84ae8b6eda
SqrtMinusOne <thexcloud@gmail.com>
2019-03-12 21:09:53 +0300

    Improved Bezier curve (not really) & Report for 4

--- src/bezier.py
+++ src/bezier.py
@@ -114,34 +114,19 @@ class MainWindow(QMainWindow, Ui_BezierWindow):
         GL.glFinish()
 
     def paintBezier(self, p1, p2, p3, p4):
-        line11, line12, line13 = [], [], []
+        self.paintBezierCommon((p1, p2, p3, p4))
 
-        # GL.glBegin(GL.GL_POINTS)
-        # GL.glVertex2d(*p4)
-        # GL.glVertex2d(*p3)
-        # GL.glEnd()
-
+    def paintBezierCommon(self, points):
+        lines = np.array([[(p2 - p1) / 100 * i + p1 for i in range(100)]
+                          for p1, p2 in zip(points, points[1:])])
+        while len(lines) > 1:
+            lines = np.array([[
+                    (p2 - p1) / 100 * i + p1 for i, p1, p2 in zip(range(100), line1, line2)
+                ]
+                for line1, line2 in zip(lines, lines[1:])
+            ])
         GL.glBegin(GL.GL_LINE_STRIP)
-
-        for i in range(100):
-            line11.append((p2 - p1) / 100 * i + p1)
-            line12.append((p3 - p2) / 100 * i + p2)
-            line13.append((p4 - p3) / 100 * i + p3)
-
-        line21, line22 = [], []
-
-        for i in range(100):
-            dot1 = line11[i]
-            dot2 = line12[i]
-            dot3 = line13[i]
-            line21.append(((dot2 - dot1) / 100 * i + dot1))
-            line22.append(((dot3 - dot2) / 100 * i + dot2))
-
-        for i in range(100):
-            dot21 = line21[i]
-            dot22 = line22[i]
-            GL.glVertex2d(*((dot22 - dot21) / 100 * i + dot21))
-
+        [GL.glVertex2d(*p) for p in lines[0]]
         GL.glEnd()
         GL.glFinish()
 
